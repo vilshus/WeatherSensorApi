2c05ea926709746b24ae0b09fb7023b07886c86c
vilshus <vil.sukauskas@gmail.com>
2021-01-23 15:06:18 +0200

    - New HTTP requests: PUT, POST, DELETE;

--- WeatherSensorApi/Controllers/WeatherSensorsController.cs
+++ WeatherSensorApi/Controllers/WeatherSensorsController.cs
@@ -25,12 +25,12 @@ namespace WeatherSensorApi.Controllers
         public ActionResult<List<ISensor>> Get()
         {
             logger.LogInformation("Root endpoint called.");
-            return SensorGet(null);
+            return GetSensor(null);
         }
 
         [Route("sensor")]
         [HttpGet]
-        public ActionResult<List<ISensor>> SensorGet(string id)
+        public ActionResult<List<ISensor>> GetSensor(string id)
         {
             if (string.IsNullOrEmpty(id))
             {
@@ -57,6 +57,75 @@ namespace WeatherSensorApi.Controllers
             }
 
             return Ok(sensor);
+        }
+
+        [Route("sensor")]
+        [HttpPut]
+        public ActionResult<string> CreateSensor(string name, string city)
+        {
+            try
+            {
+                logger.LogInformation($"Sensor creation has been requested: Name = {name}, City = {city}");
+                var sensor = sensorManager.CreateSensor(name, city);
+                logger.LogInformation($"Sensor ID = {sensor.Id} has been created.");
+                return Ok($"Sensor has been created. ID = {sensor.Id}");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [Route("sensor")]
+        [HttpPost]
+        public ActionResult<string> UpdateSensor(string id, string name, string city)
+        {
+            logger.LogInformation($"Sensor update has been requested: ID = {id} Name = {name}, City = {city}");
+
+            if (!Guid.TryParse(id, out Guid guid))
+            {
+                var message = $"Given ID {id} is in wrong format. Should be a Guid.";
+                logger.LogError(message);
+                return BadRequest(message);
+            }
+
+            try
+            {
+                if (!sensorManager.UpdateSensor(guid, name, city))
+                {
+                    var message = $"Sensor ID was not found: {id}";
+                    logger.LogError(message);
+                    return NotFound(message);
+                }
+
+                logger.LogInformation($"Sensor ID = {id} has been updated.");
+                return Ok($"Sensor has been updated. ID = {id}");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [Route("sensor")]
+        [HttpDelete]
+        public ActionResult<string> DeleteSensor(string id)
+        {
+            if (!Guid.TryParse(id, out Guid guid))
+            {
+                var message = $"Given ID {id} is in wrong format. Should be a Guid.";
+                logger.LogError(message);
+                return BadRequest(message);
+            }
+
+            if (!sensorManager.DeleteSensor(guid))
+            {
+                return NotFound($"Sensor ID = {id} was not found.");
+            }
+
+            return Ok($"Sensor has been deleted. ID = {id}");
         }
     }
 }
