393ca7e19e6eeda263e5f49841ceb5a8c585e46c
vilshus <vil.sukauskas@gmail.com>
2021-01-23 12:08:38 +0200

    - Initial commit;

---
+++ WeatherSensorApi/Controllers/WeatherSensorsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherSensorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherSensorsController : ControllerBase
+    {
+        private readonly ILogger<WeatherSensorsController> logger;
+
+        public WeatherSensorsController(ILogger<WeatherSensorsController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            logger.LogInformation("Root endpoint called.");
+            return SensorGet();
+        }
+
+        [Route("sensor")]
+        [HttpGet]
+        public string SensorGet()
+        {
+            return "Ok";
+        }
+    }
+}
---
+++ WeatherSensorApi/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherSensorApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var path = Directory.GetCurrentDirectory();
+            loggerFactory.AddFile($"{path}\\Logs\\WeatherSensorsApi.txt");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
